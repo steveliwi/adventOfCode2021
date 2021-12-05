3cb26be4da901851cbd7064dfcc7e8ae79592c0f
Steve <steve.li.steve.li@gmail.com>
2021-12-04 19:40:55 -0500

    Added Day 1 Part 2.

--- day1.cpp
+++ day1.cpp
@@ -2,29 +2,80 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <queue>
+
+bool getLineAsInt(std::ifstream& stream, int& value) {
+    std::string line;
+    if (!getline(stream, line)) {
+        return false;
+    }
+    value = std::stoi(line);
+    return true;
+}
 
 void day1()  {
     std::string line;
     std::ifstream inputStream("./data-day1.txt");
 
-    int totalMeasurements = 0;
-    bool isLastDepthDefined = false;
     int lastDepth;
-    int totalDeeperThanLast=0;
-    while (getline(inputStream, line)) {
-        int depth = std::stoi(line);
-        if (!isLastDepthDefined) {
-            lastDepth = depth;
-            isLastDepthDefined = true;
-        } else if (depth > lastDepth) {
-            ++totalDeeperThanLast;
+    getLineAsInt(inputStream, lastDepth);
+
+    int depth;
+    int deeperThanLastCounter=0;
+    while (getLineAsInt(inputStream, depth)) {
+        if (depth > lastDepth) {
+            ++deeperThanLastCounter;
         }
         lastDepth = depth;
-        ++totalMeasurements;
     }
-    inputStream.close();
 
     std::cout << "--- Day 1 ---\r\n";
-    std::cout << "totalMeasurements=" << totalMeasurements << "\r\n";
-    std::cout << "totalDeeperThanLast=" << totalDeeperThanLast << "\r\n";
+    std::cout << "deeperThanLastCounter=" << deeperThanLastCounter << "\r\n";
+}
+
+void day1_part2()  {
+    std::string line;
+    std::ifstream inputStream("./data-day1.txt");
+
+    class SlidingWindow {
+    private:
+        std::queue<int> _queue;
+        int _sum;
+    public:
+        bool push(std::ifstream& stream) {
+            int value;
+            if (!getLineAsInt(stream, value)) {
+                return false;
+            }
+            if (_queue.size() == 3) { // window size of 3
+                _sum -= _queue.front();
+                _queue.pop();
+            }
+            _queue.push(value);
+            _sum += value;
+            return true;
+        }
+        int sum() const {
+            return _sum;
+        }
+    };
+
+    SlidingWindow sw;
+    sw.push(inputStream);
+    sw.push(inputStream);
+    sw.push(inputStream);
+
+    int largerThanLastCounter = 0;
+    for (;;) {
+        int lastSum = sw.sum();
+        if (!sw.push(inputStream)) {
+            break;
+        }
+        if (sw.sum() > lastSum) {
+            ++largerThanLastCounter;
+        }
+    }
+
+    std::cout << "--- Day 1 Part 2---\r\n";
+    std::cout << "largerThanLastCounter=" << largerThanLastCounter << "\r\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-// void day1();
+void day1();
+void day1_part2();
 // void day2();
 // void day3();
-void day4();
+// void day4();
 
 int main() {
-    // day1();
+    day1();
+    day1_part2();
     // day2();
     // day3();
-    day4();
+    // day4();
     return 0;
 }
