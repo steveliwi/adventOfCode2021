63fda902ef0796ee40df05873a2d6dc620301773
Steve <steve.li.steve.li@gmail.com>
2021-12-19 13:37:37 -0500

    Implemented using priority queue.

--- day15.cpp
+++ day15.cpp
@@ -4,61 +4,75 @@
 #include <string>
 #include <vector>
 #include <limits>
-
-class VertexProxy;
+#include <queue>
 
 class Vertex {
     public:
         uint32_t row, col;
-        uint32_t cost;
-        uint32_t costFromStart;
-        // uint32_t prev_row, prev_col;
+        uint32_t vertexCost;
+        uint32_t pathCost; // cost from start to this vertex
+        Vertex* prev;
         bool isVisited;
     public:
-        Vertex(uint32_t _row, uint32_t _col, uint32_t _cost) :
+        Vertex(uint32_t _row, uint32_t _col, uint32_t _vertexCost) :
             row(_row), col(_col),
-            cost(_cost),
-            costFromStart(std::numeric_limits<uint32_t>::max()),
-            // prev_row(-1), prev_col(-1),
+            vertexCost(_vertexCost),
+            pathCost(std::numeric_limits<uint32_t>::max()),
+            prev(nullptr),
             isVisited(false) {
         }
 };
 
 typedef std::vector<std::vector<Vertex>> Grid;
 
-Vertex* minCostFromStart(Grid& grid) {
-    Vertex* minVertex = nullptr;
-    uint32_t minCostFromStart = std::numeric_limits<uint32_t>::max();
- 
-    for (size_t row = 0; row < grid.size(); ++row) {
-        for (size_t col = 0; col < grid[row].size(); ++col) {
-            Vertex* v = &grid[row][col];
-            if (!v->isVisited && v->costFromStart <= minCostFromStart) {
-                minCostFromStart = v->costFromStart;
-                minVertex = v;
-            }
+class ProxyVertex {
+    public:
+        Grid* grid;
+        Vertex* vertex;
+    private:
+        uint32_t pathCost;  // keep a local immutable copy to decouple it from the vertex's pathCost, such that priority queue order is maintained for deprecated entries.
+    public:
+        ProxyVertex(Grid* _grid, Vertex* _vertex) :
+            grid(_grid), vertex(_vertex), pathCost(_vertex->pathCost) {
         }
-    }
-    return minVertex;
-}
+        uint32_t getPathCost() const {
+            return pathCost;
+        }
+        static bool cmp(const ProxyVertex& lhs, const ProxyVertex& rhs) {
+            return lhs.pathCost > rhs.pathCost; // ascending
+        }
+};
 
 void dijkstra(Grid& grid) {
     size_t grid_max_col = grid[0].size();
     size_t grid_max_row = grid.size();
 
-    grid[0][0].costFromStart = 0;
+    grid[0][0].pathCost = 0;
+
+    typedef std::priority_queue<ProxyVertex, std::vector<ProxyVertex>, decltype(&ProxyVertex::cmp)> PriorityQueue;
+    PriorityQueue q(ProxyVertex::cmp);
+
+    // initialize queue
+    for (int row=0; row<grid_max_row; ++row) {
+        for (int col=0; col<grid_max_col; ++col) {
+            q.emplace(&grid, &grid[row][col]);
+        }
+    }
 
     int lastPercent = -1;
-    std::cout << "percent=";
+    std::cout << "progress : ";
 
     uint32_t numVisited = 0;
-    for (; ;) {
-        // can be optimized with a priority queue
-        Vertex* uPtr = minCostFromStart(grid);
-        if (uPtr == nullptr) {
-            break; // all visited
+    while (!q.empty()) {
+        const ProxyVertex& proxy = q.top();
+        Vertex& u = *proxy.vertex;
+        bool isProxyDeprecated = (proxy.getPathCost() != u.pathCost);
+        q.pop();
+
+        if (isProxyDeprecated) {
+            continue;
         }
-        Vertex& u = *uPtr;
+
         u.isVisited = true;
         ++numVisited;
 
@@ -89,27 +103,44 @@ void dijkstra(Grid& grid) {
         }
 
         for (auto v : neighbours) {
-            uint32_t alt = u.costFromStart + v->cost;
-            if (alt < v->costFromStart) {
-                v->costFromStart = alt;
-                // v->prev_row = u.row;
-                // v->prev_col = u.col;
+            uint32_t alt = u.pathCost + v->vertexCost;
+            if (alt < v->pathCost) {
+                v->pathCost = alt;
+                v->prev = &u;
+                q.emplace(&grid, v);
             }
+        }
+
+        // progress indicator
+        int percent = (numVisited * 100) / grid_max_col / grid_max_row;
+        if (lastPercent != percent) {
+            if ((percent % 10) == 0) {
+                std::cout << (percent / 10);
+             } else {
+                 std::cout << ".";
+             }
+            lastPercent = percent;
         }
 
         // stop when target has reached
         if ((u.row == grid_max_row - 1) && (u.col == grid_max_col - 1)) {
             break;
         }
-
-        // serves as an indicator for part 2
-        int percent = (numVisited * 100) / grid_max_col / grid_max_row;
-        if (lastPercent != percent) {
-            std::cout << percent << " ";
-            lastPercent = percent;
-        }
     }
     std::cout << std::endl;
+}
+
+void displayGrid(const Grid& grid) {
+    size_t grid_max_col = grid[0].size();
+    size_t grid_max_row = grid.size();
+
+    for (size_t row=0; row<grid_max_row; ++row) {
+        for (size_t col=0; col<grid_max_col; ++col) {
+            const Vertex& u = grid[row][col];
+            std::cout << u.pathCost << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 void day15() {
@@ -139,7 +170,7 @@ void day15() {
             std::vector<Vertex>& rowOfVertices = grid2[row];
             for (int col=0; col<grid_max_col; ++col) {
                 const Vertex& src = rowOfVertices[(n * grid_max_col) + col];
-                Vertex dst(row, ((n + 1) * grid_max_col) + col, (src.cost < 9) ? (src.cost + 1) : 1);
+                Vertex dst(row, ((n + 1) * grid_max_col) + col, (src.vertexCost < 9) ? (src.vertexCost + 1) : 1);
                 rowOfVertices.push_back(dst);
             }
         }
@@ -151,7 +182,7 @@ void day15() {
             std::vector<Vertex> dstRow;
             for (int col=0; col<srcRow.size(); ++col) {
                 const Vertex& src = srcRow[col];
-                Vertex dst(((n + 1) * grid_max_row) + row, col, (src.cost < 9) ? (src.cost + 1) : 1);
+                Vertex dst(((n + 1) * grid_max_row) + row, col, (src.vertexCost < 9) ? (src.vertexCost + 1) : 1);
                 dstRow.push_back(dst);
             }
             grid2.emplace_back(dstRow);
@@ -164,17 +195,19 @@ void day15() {
         std::cout << "--- Day 15 ---" << std::endl;
 
         dijkstra(grid);
-        Vertex& dst = grid[grid_max_row-1][grid_max_col-1];
+        //displayGrid(grid);
 
-        std::cout << "lowest costFromStart = " << dst.costFromStart << std::endl;
+        Vertex& dst = grid[grid_max_row-1][grid_max_col-1];
+        std::cout << "lowest pathCost = " << dst.pathCost << std::endl;
     }
 
     {
         std::cout << "--- Day 15 Part 2 ---" << std::endl;
 
         dijkstra(grid2);
-        Vertex& dst = grid2[grid2_max_row-1][grid2_max_col-1];
+        // displayGrid(grid2);
 
-        std::cout << "lowest costFromStart = " << dst.costFromStart << std::endl;
+        Vertex& dst = grid2[grid2_max_row-1][grid2_max_col-1];
+        std::cout << "lowest pathCost = " << dst.pathCost << std::endl;
     }
 }
